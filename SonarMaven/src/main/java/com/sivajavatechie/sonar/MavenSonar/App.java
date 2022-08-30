02827d77a344e7a87fbb05fd3e2152e425ae4cf2
Dwarakesh T <105031924+dwarakesht@users.noreply.github.com>
2022-08-30 14:55:21 +0530

    Add files via upload

---
+++ SonarMaven/src/main/java/com/sivajavatechie/sonar/MavenSonar/App.java
@@ -0,0 +1,16 @@
+package com.sivajavatechie.sonar.MavenSonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private int b = 90;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        App app = null;
+        System.out.println(app.b);
+    }
+}
